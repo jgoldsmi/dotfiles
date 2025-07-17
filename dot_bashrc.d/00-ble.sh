f83f7d2e2e9f191f9ddec6b7c913173cb13207ed
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2025-07-16 21:39:18 -0400

    Satisfy the shell linter with returns after pushd/popd

--- dot_bashrc.d/00-ble.sh
+++ dot_bashrc.d/00-ble.sh
@@ -1,8 +1,8 @@
 if [[ ! -f ~/.local/share/blesh/ble.sh ]]; then
-  pushd /tmp
+  pushd /tmp || return
   git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
   make -C ble.sh install PREFIX=~/.local
-  popd
+  popd || return
 fi
 source ~/.local/share/blesh/ble.sh
 bleopt color_scheme=catppuccin_mocha
